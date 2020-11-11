f525412ba6b1f785a3e181ba4cff2faf0b325343
Andrey <andrnovip@gmail.com>
2020-11-11 19:11:17 +0300

    start the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
+/*
+ *  Main page of appex-system app
+ *  Author: Andrey ( https://github.com/andaran )
+ *  Development started on 11.11.2020 19:10
+ *  
+ */
+
+/* React */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+/* Components */
+
+/* Libraries */
+
 import * as serviceWorker from './serviceWorker';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import {Provider} from 'react-redux';
+import store from './store';
+
+/*   ---==== Render ====---   */
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <Nav/>
+      <Switch>
+        <Route exact path="/">
+          <Main func = { null }/>
+        </Route>
+        <Route exact path="/articles" component={ Articles }/>
+        <Route exact path="/articles/:articleID" component={ ArticlePage }/>
+        <Route exact component={Error}/>
+      </Switch>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
