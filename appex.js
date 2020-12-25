f1207d380ede44025818cbf1ee06fa490b9f09a4
Andrey <andrnovip@gmail.com>
2020-12-25 20:27:21 +0300

    add DB

--- appex.js
+++ appex.js
@@ -5,8 +5,24 @@ const express = require("express");
 const socketIo = require("socket.io");
 const path = require("path");
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+
+/*   ---==== Connect to DB ====---   */
+
+mongoose.connect('mongodb://127.0.0.1', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, err => err ? console.error(err) : console.log('[+] База подключена!'));
+
+
+
+/*   ---==== Express server ====---   */
 
 const app = express();
+app.use(bodyParser.json());
 
 /* set static dir */
 app.use(express.static(path.join(__dirname, 'build')));
@@ -22,7 +38,8 @@ app.get('*', (req, res) => {
 
 
 
-/* sockets */
+/*   ---==== Use socket.io ====---   */
+
 const io = socketIo(server);
 io.on('connection', (socket) => {
   socket.on('test', data => {
