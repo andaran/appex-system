e3f17ce666a8a9224501da25457b792f683f1390
Andrey <andrnovip@gmail.com>
2021-03-05 14:18:55 +0300

    add settings design

--- src/Components/main/system-apps/settings/SettingsApp.jsx
+++ src/Components/main/system-apps/settings/SettingsApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faLock, faAt, faUserSlash, faMobile } from "@fortawesome/free-solid-svg-icons";
 
 /* Component */
 export default class SettingsApp extends React.Component {
@@ -9,8 +9,38 @@ export default class SettingsApp extends React.Component {
   render() {
 
     return (
-      <div>
-        test
+      <div className="main-settings-wrap">
+        <div className="main-settings-title">Настройки</div>
+        <ul className="settings-list">
+          <li className="apps-params">
+            <FontAwesomeIcon icon={ faUser } style={{ color: '#1abc9c' }}/>
+            <span> Сменить имя пользователя </span>
+            <div/>
+          </li>
+          <li className="apps-params">
+            <FontAwesomeIcon icon={ faLock } style={{ color: '#3498db' }}/>
+            <span> Сменить пароль </span>
+            <div/>
+          </li>
+          <li className="apps-params">
+            <FontAwesomeIcon icon={ faAt } style={{ color: '#e67e22' }}/>
+            <span> Сменить почту </span>
+            <div/>
+          </li>
+          <li className="apps-params">
+            <FontAwesomeIcon icon={ faUserSlash } style={{ color: '#e74c3c' }}/>
+            <span> Удалить аккаунт </span>
+            <div/>
+          </li>
+        </ul>
+        <hr className="settings-line"/>
+        <ul className="settings-list">
+          <li className="apps-params">
+            <FontAwesomeIcon icon={ faMobile } style={{ color: '#9b59b6' }}/>
+            <span> Настройки приложений </span>
+            <div/>
+          </li>
+        </ul>
       </div>
     );
   }
