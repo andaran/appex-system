f5b1ca058cb12e490fe9c0fbc1ab4b7e7cf05027
Andrey <andrnovip@gmail.com>
2021-04-22 21:07:11 +0300

    add deleting and downloading apps

--- src/Components/main/system-apps/store/AppDownload.jsx
+++ src/Components/main/system-apps/store/AppDownload.jsx
@@ -5,6 +5,7 @@ import * as Icons from "@fortawesome/free-solid-svg-icons";
 import {faIdBadge, faKey, faSearch} from "@fortawesome/free-solid-svg-icons";
 import {fetchUser} from "../../../../actions/userActions";
 import {connect} from "react-redux";
+import {request} from "../../../../tools/apiRequest/apiRequest";
 
 /* Component */
 class AppDownload extends React.Component {
@@ -13,9 +14,29 @@ class AppDownload extends React.Component {
 
     // bind
     this.download = this.download.bind(this);
+    this.delete = this.delete.bind(this);
   }
 
   render() {
+
+    const id = this.props.id;
+    const app = this.props.user.installedApps.find(elem => elem.id === id);
+
+    let button = <div className="reg-window__input-item">
+      <div className="reg-window__button reg-window__button_green" onClick={ this.download }>
+        Скачать
+      </div>
+    </div>;
+
+    console.log('APP', app);
+    if (app) {
+      button = <div className="reg-window__input-item">
+        <div className="reg-window__button reg-window__button_red" onClick={ this.delete }>
+          Удалить
+        </div>
+      </div>;
+    }
+
     return (
       <div className="app-block">
         <div className="reg-window__input-item">
@@ -28,11 +49,7 @@ class AppDownload extends React.Component {
             <input type="text" readOnly={ true } value={ this.props.title } className="reg-window__input"/>
           </div>
         </div>
-        <div className="reg-window__input-item">
-          <div className="reg-window__button reg-window__button_green" id="reg-btn" onClick={ this.send }>
-            Скачать
-          </div>
-        </div>
+        { button }
       </div>
     );
   }
@@ -41,8 +58,51 @@ class AppDownload extends React.Component {
 
   }
 
+  delete() {
+
+    const id = this.props.id;
+    const app = this.props.user.installedApps.find(elem => elem.id === id);
+    const newApps = this.props.user.installedApps;
+
+    /* delete app */
+    const index = newApps.indexOf(app);
+    if (index > -1) {
+      newApps.splice(index, 1);
+    }
+
+    /* do request */
+    this.userRequest({ installedApps: newApps });
+    this.forceUpdate();
+  }
+
   download() {
 
+    const id = this.props.id;
+    const title = this.props.title;
+    const color = this.props.color;
+    const icon = this.props.icon;
+    const newApps = this.props.user.installedApps;
+
+    newApps.push({
+      title, icon, color, id,
+    });
+
+    /* do request */
+    this.userRequest({ installedApps: newApps });
+    this.forceUpdate();
+  }
+
+  userRequest(params) {
+    request(`/api/change_user`, params)
+      .then(res => res.json()).then(body => {
+      if (body.status === 'ok') {
+        this.props.fetchUser();
+      } else {
+        console.log(body);
+      }
+    }).catch(err => {
+      console.error('Ошибка запроса!');
+    });
   }
 }
 
